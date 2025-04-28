4cae5cbd92ce0f3c21cdf328ba4e556ed3fba97d
HistidineDwarf <dvorakdwarf@gmail.com>
2025-04-28 12:10:06 -0700

    Game is winnable now and it puts a big X or O for the winner

--- client/src/Board.jsx
+++ client/src/Board.jsx
@@ -1,24 +1,67 @@
 import React from 'react';
+import { useState } from 'react';
 import Cell from './Cell.jsx';
 
 function Board() {
-  return <div className='board'>
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isWon, setIsWon] = useState(null);
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+
+    if (isWon != null) { return; }
+    if (nextSquares[i] != null) { return; }
+
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setXIsNext(!xIsNext);
+    setSquares(nextSquares);
+
+    setIsWon(calculateWinner(nextSquares));
+  }
+
+  return <div className="board">
+  <div className='win-container'><span className={isWon}>{isWon}</span></div>
   <div className='board-row top-row'>
-    <Cell/>
-    <Cell/>
-    <Cell/>
+    <Cell value={squares[0]} onSquareClick={() => handleClick(0)}/>
+    <Cell value={squares[1]} onSquareClick={() => handleClick(1)}/>
+    <Cell value={squares[2]} onSquareClick={() => handleClick(2)}/>
   </div>
   <div className='board-row center-row'>
-    <Cell/>
-    <Cell/>
-    <Cell/>
+    <Cell value={squares[3]} onSquareClick={() => handleClick(3)}/>
+    <Cell value={squares[4]} onSquareClick={() => handleClick(4)}/>
+    <Cell value={squares[5]} onSquareClick={() => handleClick(5)}/>
   </div>
   <div className='board-row bottom-row'>
-    <Cell/>
-    <Cell/>
-    <Cell/>
+    <Cell value={squares[6]} onSquareClick={() => handleClick(6)}/>
+    <Cell value={squares[7]} onSquareClick={() => handleClick(7)}/>
+    <Cell value={squares[8]} onSquareClick={() => handleClick(8)}/>
   </div>
   </div>;
 };
 
 export default Board;
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
--- client/src/Cell.jsx
+++ client/src/Cell.jsx
@@ -1,19 +1,8 @@
 import React from 'react';
-import { useState } from 'react';
 
-function Cell() {
-  const [value, setValue] = useState('O');
-
-  function handleClick() {
-    if (value === 'O') {
-      setValue('X');
-    } else {
-      setValue('O')
-    }
-  }
-
+function Cell({value, onSquareClick}) {
   return <>
-    <button className='cell' onClick={handleClick}>{value}</button>
+    <button className='cell' onClick={onSquareClick}>{value}</button>
   </>;
 };
 
