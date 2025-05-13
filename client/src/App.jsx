e1d2b831bcfa574e2067f9e6b2200891a8201105
HistidineDwarf <dvorakdwarf@gmail.com>
2025-05-13 11:49:11 -0700

    Made hotkeys for movement but I am not happy. Going to use something else

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { useState } from 'react';
-import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import { useHotkeys, isHotkeyPressed } from 'react-hotkeys-hook';
+import { TransformWrapper, TransformComponent, useControls } from "react-zoom-pan-pinch";
 import Board from './Board.jsx';
 
 function initBoard(depth) {
@@ -13,9 +14,27 @@ function initBoard(depth) {
   }
 }
 
+function get_pos() {
+  let element = document.querySelector(".react-transform-component");
+  let x_pos = Number(element.style["transform"].split("px")[0].split("(")[1]);
+  let y_pos = Number(element.style["transform"].split("px")[1].split(",")[1]);
+
+  let scale_idx = element.style["transform"].indexOf("scale(");
+  let scale = Number(element.style["transform"].substring(scale_idx+6).split(")")[0])
+
+  console.log(element.style["transform"].split("px"));
+  console.log(scale);
+
+  if (isNaN(x_pos)) { x_pos = 0 }
+  if (isNaN(y_pos)) { y_pos = 0 }
+
+  return [x_pos, y_pos, scale];
+}
+
 function App() {
   const [xIsNext, setXIsNext] = useState(true);
   const [isWon, setIsWon] = useState(null);
+  const { setTransform } = useControls();
   const depth = 1;
 
   const boardSize = 75;
@@ -23,7 +42,8 @@ function App() {
 
   const cssVars = {
     "--board-size": boardSize + "px",
-    "--border-size": borderSize + "px"
+    "--border-size": borderSize + "px",
+    "width": "fit-content",
   }
 
   const wrapper = (winningPlayer) => {
@@ -31,19 +51,35 @@ function App() {
     console.log(winningPlayer);
   }
 
-  let state = initBoard(depth);
+  // let state = initBoard(depth);
+  //Kinda hacky? I don't think zoom-pan-pinch lets you increment transform by itself
+  useHotkeys('w', () => {
+    // if (isHotkeyPressed('w') == false) { return; }
 
+    let [x_pos, y_pos, scale] = get_pos();    
+    setTransform(x_pos, y_pos+10, scale);
+  });
+  useHotkeys('a', () => {
+    // if (isHotkeyPressed('w') == false) { return; }
+
+    let [x_pos, y_pos, scale] = get_pos();    
+    setTransform(x_pos+10, y_pos, scale);
+  });
+  useHotkeys('s', () => {
+    // if (isHotkeyPressed('w') == false) { return; }
+
+    let [x_pos, y_pos, scale] = get_pos();    
+    setTransform(x_pos, y_pos-10, scale);
+  });
+  useHotkeys('d', () => {
+    // if (isHotkeyPressed('w') == false) { return; }
+
+    let [x_pos, y_pos, scale] = get_pos();    
+    setTransform(x_pos-10, y_pos, scale);
+  });
+  
   return <div style={cssVars}>
-  <TransformWrapper
-    initialScale={1}
-    maxScale={2}
-    minScale={1}
-    centerOnInit={true}
-  >
-    <TransformComponent>
     <Board depth={depth} xIsNext={xIsNext} setXIsNext={setXIsNext} externalSetIsWon={wrapper}/>
-    </TransformComponent>
-  </TransformWrapper>
   </div>;
 };
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import StrictMode from 'react';
-import App from './App.jsx';
+import ZoomViewWrapper from './ZoomViewWrapper.jsx';
 import './styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <ZoomViewWrapper />
 );
