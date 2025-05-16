fe5da2ed2ebd0d27e6cd86b0564cf3c82082bd21
HistidineDwarf <dvorakdwarf@gmail.com>
2025-05-15 23:16:01 -0700

    Gave up on WASD. Next stop is adding an init property to boards and switching between boards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,43 +5,25 @@ import { TransformWrapper, TransformComponent, useControls } from "react-zoom-pa
 import Board from './Board.jsx';
 
 function initBoard(depth) {
-  let state = {
-
+  let state = [];
+  for (let i = 0; i < 9; i++) {
+    if (depth > 0) {
+      state[i] = initBoard(depth-1)
+    } else {
+      state[i] = null;
+    }
   }
 
-  for (let i = depth; i >= 0; i++) {
-    // state[]
-  }
-}
-
-function get_pos() {
-  let element = document.querySelector(".react-transform-component");
-  let x_pos = Number(element.style["transform"].split("px")[0].split("(")[1]);
-  let y_pos = Number(element.style["transform"].split("px")[1].split(",")[1]);
-
-  let scale_idx = element.style["transform"].indexOf("scale(");
-  let scale = Number(element.style["transform"].substring(scale_idx+6).split(")")[0])
-
-  console.log(element.style["transform"].split("px"));
-  console.log(scale);
-
-  if (isNaN(x_pos)) { x_pos = 0 }
-  if (isNaN(y_pos)) { y_pos = 0 }
-
-  return [x_pos, y_pos, scale];
+  return state
 }
 
 function App() {
   const [xIsNext, setXIsNext] = useState(true);
   const [isWon, setIsWon] = useState(null);
-  const [isHeld, setIsHeld] = useState({
-    "w": false,
-    "a": false,
-    "s": false,
-    "d": false
-  });
-  const { setTransform } = useControls();
   const depth = 1;
+
+  let current_depth = depth;
+  let current_index = 0;
 
   const boardSize = 75;
   const borderSize = 2;
@@ -57,37 +39,14 @@ function App() {
     console.log(winningPlayer);
   }
 
-  // let state = initBoard(depth);
-  
-  addEventListener("keydown", (event) => { 
-    let newIsHeld = isHeld;
-    newIsHeld[event.key] = true; 
-    setIsHeld(newIsHeld);
-  });
-  addEventListener("keyup", (event) => { 
-    let newIsHeld = isHeld;
-    newIsHeld[event.key] = false; 
-    setIsHeld(newIsHeld);
-  });
+  let state = initBoard(depth);
+  console.log(state);
 
-  setInterval(() => {
-    //See if this lags out
-    // let [x_pos, y_pos, scale] = get_pos();    
+  useHotkeys('w', () => {
+    if (current_depth == dept && current_index < 3) { return }
 
-    if (isHeld["w"] == true) {
-      let [x_pos, y_pos, scale] = get_pos();    
-      setTransform(x_pos, y_pos+10, scale);
-    } else if (isHeld["a"] == true) {
-      let [x_pos, y_pos, scale] = get_pos();    
-      setTransform(x_pos+10, y_pos, scale);
-    } else if (isHeld["s"] == true) {
-      let [x_pos, y_pos, scale] = get_pos();    
-      setTransform(x_pos, y_pos-10, scale);
-    } else if (isHeld["d"] == true) {
-      let [x_pos, y_pos, scale] = get_pos();    
-      setTransform(x_pos-10, y_pos, scale);
-    }
-  }, 100);
+    current_index -= 3;
+  });
 
   return <div style={cssVars}>
     <Board depth={depth} xIsNext={xIsNext} setXIsNext={setXIsNext} externalSetIsWon={wrapper}/>
