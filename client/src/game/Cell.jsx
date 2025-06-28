dbe3546a21ac8208780ba2451aca2a84c7e6377c
HistidineDwarf <dvorakdwarf@gmail.com>
2025-06-27 17:13:34 -0700

    Middle SVGs look alright mostly, yet to try flex-grow. Might fix boards losing 1/1 aspect ratio higher up

--- client/src/game/Cell.jsx
+++ client/src/game/Cell.jsx
@@ -3,19 +3,11 @@ import React from 'react';
 import cross from '../assets/Thick_Cross.svg' ;
 import circle from '../assets/Thick_Circle.svg';
 
-//Should try to remove
-const gameStateStyle={
-  "width": "100%",
-  "height": "auto",
-  // "top": (-8 + (16*depth)) + "px",
-  "top": "0px",
-}
-
 function get_image(value) {
   if (value === "X") {
-    return <img src={cross} className="move" style={gameStateStyle}/>;
+    return <img src={cross} className="move"/>;
   } else if (value == "O") {
-    return <img src={circle} className="move" style={gameStateStyle}/>;
+    return <img src={circle} className="move"/>;
   }
 
   //If empty
