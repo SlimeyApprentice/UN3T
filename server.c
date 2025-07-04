39e12d6e522c890248792a1176ede9632964ebd4
James Knepper <jamesknepper03@gmail.com>
2025-07-03 20:09:10 -0700

    add a few lines to server.c

--- server.c
+++ server.c
@@ -1,5 +1,9 @@
 #include "server.h"
 
 ServerData *init_server() {
-    
+    ServerData *server = malloc(sizeof(ServerData));
+    if (!server) {
+	    printf("Malloc Failed :(");
+	    exit(-1);
+    }
 }
