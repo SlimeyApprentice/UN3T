2b182157b69ae73a460c25a76fc43949b8ac23fc
HistidineDwarf <dvorakdwarf@gmail.com>
2025-08-28 22:16:13 -0700

    Succesfully connected and sent a message

--- lib/server.c
+++ lib/server.c
@@ -315,8 +315,10 @@ void process_request(ServerData *server, Connections *client) {
 
 int handle_callback(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len) {
 	Connections *client = user;
-	ServerData *server = lws_protocol_vh_priv_get(lws_get_vhost(wsi), lws_get_protocol(wsi));
+	ServerData *server = lws_protocol_vh_priv_zalloc(lws_get_vhost(wsi), lws_get_protocol(wsi), sizeof(ServerData));
 
+	printf("reason: %d\n", reason);
+	
 	switch (reason) {
 		case LWS_CALLBACK_PROTOCOL_INIT:
 			ServerData *init_server = lws_protocol_vh_priv_zalloc(lws_get_vhost(wsi), lws_get_protocol(wsi), sizeof(ServerData));
