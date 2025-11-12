ca55fa39d75a8717c51052438b3862b68438f437
HistidineDwarf <dvorakdwarf@gmail.com>
2025-11-11 17:27:34 -0800

    To push

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import {HashRouter, Routes, Route} from "react-router-dom";
 import useWebSocket from "react-use-websocket";
 import { Provider } from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react'
 
 import type { Connection } from "./serverInterface.ts";
 import Game from "./components/game/Game.tsx";
@@ -28,12 +29,14 @@ function App() {
   }
 
   return <Provider store={store}>
+      <PersistGate loading={null}>
       <HashRouter>
         <Routes>
           <Route path="/" element={<Home connection={connection}/>}></Route>
           <Route path="/game" element={<Game connection={connection}/>}></Route>
         </Routes>
       </HashRouter>
+      </PersistGate>
   </Provider>;
 };
 
