ce5d9105356ca431f9f5d1a0763df99bba819581
HistidineDwarf <dvorakdwarf@gmail.com>
2025-11-13 20:55:22 -0800

    Revert "To push"
    This reverts commit ca55fa39d75a8717c51052438b3862b68438f437.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import {HashRouter, Routes, Route} from "react-router-dom";
 import useWebSocket from "react-use-websocket";
 import { Provider } from "react-redux";
-import { PersistGate } from 'redux-persist/integration/react'
 
 import type { Connection } from "./serverInterface.ts";
 import Game from "./components/game/Game.tsx";
@@ -33,14 +32,12 @@ function App() {
   }
 
   return <Provider store={store}>
-      <PersistGate loading={null}>
       <HashRouter>
         <Routes>
           <Route path="/" element={<Home connection={connection}/>}></Route>
           <Route path="/game" element={<Game connection={connection}/>}></Route>
         </Routes>
       </HashRouter>
-      </PersistGate>
   </Provider>;
 };
 
