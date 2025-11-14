598fd560e8735024b272baccca5ad6bda5a00dc7
HistidineDwarf <dvorakdwarf@gmail.com>
2025-11-13 20:50:20 -0800

    Revert "Basic persist"
    This reverts commit 140ec713ea4c0dc9483b2fe93bee3c2b883e4871.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,11 @@ import { PersistGate } from 'redux-persist/integration/react'
 import type { Connection } from "./serverInterface.ts";
 import Game from "./components/game/Game.tsx";
 import Home from "./components/home/Home.tsx"
+<<<<<<< HEAD
 import getStore from "./state/store.ts";
+=======
+import store from "./state/store.ts";
+>>>>>>> parent of 140ec71 (Basic persist)
 
 function App() {
   const { 
@@ -28,10 +32,8 @@ function App() {
       readyState
   }
 
-  const { store, persistor } = getStore();
-
   return <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      <PersistGate loading={null}>
       <HashRouter>
         <Routes>
           <Route path="/" element={<Home connection={connection}/>}></Route>
--- client/src/state/store.ts
+++ client/src/state/store.ts
@@ -1,70 +1,19 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import { persistStore, persistReducer, type PersistConfig } from 'redux-persist'
-import sessionStorage from 'redux-persist/lib/storage/session'
-import type { PersistPartial } from 'redux-persist/es/persistReducer';
+import { configureStore } from '@reduxjs/toolkit'
 
-import type { ControlState, GameState } from './types.ts';
 import gameReducer from './gameSlice.ts'
 import controlReducer from './controlSlice.ts'
-import hardSet from 'redux-persist/lib/stateReconciler/hardSet'
+import type { ControlState, GameState } from './types.ts';
 
 export type RootState = {
   game: GameState;
   control: ControlState;
-} & PersistPartial;
-
-const rootPersistConfig = {
-  key: 'root',
-  storage: sessionStorage,
-  whitelist: [],
-  // stateReconciler: hardSet,
 };
 
-const gamePersistConfig = {
-  key: 'game',
-  storage: sessionStorage,
-  whitelist: ['id'],
-  // blacklist: ['globalBoard'],
-  // whitelist: [],
-  stateReconciler: hardSet,
-}
+const store = configureStore({
+  reducer: {
+    game: gameReducer,
+    control: controlReducer,
+  },
+})
 
-const controlPersistConfig = {
-  key: 'control',
-  storage: sessionStorage,
-  // whitelist: ['current_depth'],
-  whitelist: [],
-}
-
-const rootReducer = combineReducers({
-  game: persistReducer(gamePersistConfig, gameReducer), 
-  control: persistReducer(controlPersistConfig, controlReducer)
-});
-
-const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
-
-export default function getStore() {
-  let store = configureStore({ reducer: persistedReducer });
-  let persistor = persistStore(store);
-  return { store, persistor }
-}
-
-// import { configureStore } from '@reduxjs/toolkit'
-
-// import gameReducer from './gameSlice.ts'
-// import controlReducer from './controlSlice.ts'
-// import type { ControlState, GameState } from './types.ts';
-
-// export type RootState = {
-//   game: GameState;
-//   control: ControlState;
-// };
-
-// const store = configureStore({
-//   reducer: {
-//     game: gameReducer,
-//     control: controlReducer,
-//   },
-// })
-
-// export default store
+export default store
