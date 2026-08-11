0422dd9deb13c1f53332a25f8210e483afa153a2
James Knepper <jamesknepper03@gmail.com>
2026-08-10 21:13:52 -0700

    client now points to global server, not local

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
     lastMessage, 
     lastJsonMessage, 
     readyState 
-  } = useWebSocket("ws://localhost:8332", {
+  } = useWebSocket("ws://treebeard.ddns.net:8332", {
       protocols: "UN3T",
       //Will attempt to reconnect on all close events, such as server shutting down
       shouldReconnect: (_closeEvent) => true,
@@ -33,6 +33,7 @@ function App() {
         playing 
         loop 
         className="off" 
+	volume = {0.5}
         src="https://www.youtube.com/playlist?list=PLZ0r2gDOqlXU"
       />
       <HashRouter>
